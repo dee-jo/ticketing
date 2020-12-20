b1bddaca583f456aec88bbb52f5ab059a0f636b0
sapphirepencil <djhawro@gmail.com>
2020-12-20 19:16:00 +0000

    Up to lesson 139: connecting to MongoDb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,48 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose'
 
 const app = express();
 app.use(json());
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-})
+app.all('/*', async () => {
+  throw new NotFoundError();
+});
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!');
-})
+app.use(errorHandler); // middleware setup needs to be after global route def, otherwise won't work!!!!
+ 
+// app.post('/api/users/test', (req, res) => {
+//   console.log('received GET request')
+//   res.status(201).send({});
+// })
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    console.log('Connected to MongoDb');
+  }
+  catch (err) {
+    console.log(err)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!');
+  });
+}
+start();
+
---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,22 @@
+import { CustomError } from '../errors/custom-error';
+import { Request, Response, NextFunction } from 'express';  
+  
+// when express sees 4 arguments in a function passed to app.use(), it categorizes it as error handler
+export const errorHandler = (
+  err: Error, 
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    // console.log('[error-middleware] Handling RequestValidationError');
+    res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  // else {
+  //   res.status(400).send({
+  //     errors: [
+  //       { message: 'Something went wrong'}
+  //     ]
+  //   })
+  // } 
+}
